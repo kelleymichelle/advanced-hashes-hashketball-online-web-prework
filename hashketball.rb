ba0df08ba72161a346c28013f828a6e67378372b
Kelley Chaplain <kmchaplain@gmail.com>
2020-07-19 20:58:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -118,7 +118,7 @@ end
 def num_points_scored(player)
   points = 0
   game_hash.each do |key, value|
-    value[:players].each do |k, v|
+    value[:players].each do |player|
       binding.pry
     end
   end
