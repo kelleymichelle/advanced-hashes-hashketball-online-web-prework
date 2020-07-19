18ca94614be419af71e4b15d8c6715136d7ddfd6
Kelley Chaplain <kmchaplain@gmail.com>
2020-07-19 20:48:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -115,3 +115,31 @@ def game_hash
   }
 end
 
+def num_points_scored
+  
+end
+
+def shoe_size
+  
+end
+
+def team_colors
+  
+end
+
+def team_names
+  
+end
+
+def player_numbers
+  
+end
+
+def player_stats
+  
+end
+
+def big_shoe_rebounds
+  
+end
+
